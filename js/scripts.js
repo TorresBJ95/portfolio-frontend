98078dc3f077a077037e342404e5ffb83223d8e5
Brayan <torresbrayan70@gmail.com>
2022-09-30 11:25:32 -0300

    update javascript functions edit text on index

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,21 @@ function cambiar_parrafo(){
 function myFunction2(valor){
     document.getElementById("text-aboutme").innerText=valor;
 };
+
+function logMessage(message){
+    console.log(message + "<br>");
+}
+
+//controla si se presiono un enter
+let textarea=document.getElementById("edit-aboutme")
+textarea.addEventListener('keyup', (e) => {
+    logMessage(`Key "${e.key}" released [event: keyup]`);
+    if (e.key=="Enter"){
+        document.getElementById("edit-aboutme").style.display="none"
+    }
+    
+});
+
+
+
+
