6aacd78d4b61c27e55789b53a4636f22ce66e297
Brayan <torresbrayan70@gmail.com>
2022-10-31 13:41:14 -0300

    final edit

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,25 @@
-function cambiar_parrafo(){
-    document.getElementById("edit-aboutme").style.display="inline";
-    let texto = document.getElementById("text-aboutme").innerText;
-    console.log(texto);
-};
+function cambiar_parrafo() {
+  document.querySelector("input").style.display = "inline";
+  let container = document.querySelector("#section");
+  let texto = container.querySelectorAll("div > p");
+  for (let i = 0; i < texto.length; i++) {
+    texto[i].textContent = "";
+  }
+}
+function asignar_palabras(value) {
+  const cambio_texto = document.getElementById("text-aboutme");
+  cambio_texto.textContent = value;
+}
 
-function myFunction2(valor){
-    document.getElementById("text-aboutme").innerText=valor;
-};
-
-function logMessage(message){
-    console.log(message + "<br>");
+function logMessage(message) {
+  console.log(message + "<br>");
 }
 
 //controla si se presiono un enter
 let textarea = document.getElementById("edit-aboutme");
-textarea.addEventListener('keyup', (e) => {
-    logMessage(`Key "${e.key}" released [event: keyup]`);
-    if (e.key=="Enter"){
-        document.getElementById("edit-aboutme").style.display="none"
-    }
-    
+textarea.addEventListener("keyup", (e) => {
+  logMessage(`Key "${e.key}" released [event: keyup]`);
+  if (e.key == "Enter") {
+    document.getElementById("edit-aboutme").style.display = "none";
+  }
 });
-
-
-
-
